4b553c1e49a8775f4b50caedf0e86d5de27eff05
spiritmonster <suryansh.anand007@gmail.com>
2020-03-21 17:33:30 +0530

    Commit 1

---
+++ Prime.py
@@ -0,0 +1,24 @@
+import math
+num = int(input("Enter the number to be checked :- "))
+
+
+def is_prime2(num):
+
+    """
+    Better method of checking for primes.
+    """
+    if num == 1:
+        return "None"
+    elif num == 3 or num == 2:
+        print(True)
+    elif num % 2 == 0:
+        print(False)
+    else:
+        for i in range(3,int(math.sqrt(num))+1,2):
+            if num % i == 0:
+                print(False)
+                exit()
+        print(True)
+
+
+is_prime2(num)
